d8c8fa0ba60dc5dd882b93f0c743c5efb3541540
Andy Sawyer <96326877+nydasco@users.noreply.github.com>
2024-12-13 12:30:03 +1100

    Add dim_employee and associated changes

--- models/facts/fct_sale.sql
+++ models/facts/fct_sale.sql
@@ -13,6 +13,16 @@ d_client AS (
         {{ ref('dim_client') }}
 ),
 
+d_employee AS (
+    SELECT
+        _employee_hk,
+        _employee_bk,
+        _valid_from_datetime,
+        _valid_to_datetime
+    FROM
+        {{ ref('dim_employee') }}
+),
+
 d_region AS (
     SELECT
         _region_hk,
@@ -24,6 +34,7 @@ d_region AS (
 sale AS (
     SELECT
         COALESCE(d_client._client_hk, '-2') AS _client_hk,
+        COALESCE(d_employee._employee_hk, -2) AS _employee_hk,
         COALESCE(d_region._region_hk, '-2') AS _region_hk,
         sale.date::DATE AS sale_date,
         SUM(sale.sale) AS sale_amount
@@ -31,6 +42,10 @@ sale AS (
         {{ ref('stg_sale') }} AS sale
         LEFT JOIN d_client
             ON COALESCE(sale.client_id, '-1') = d_client._client_bk
+        INNER JOIN d_employee
+            ON COALESCE(sale.employee_id, '-1') = d_employee._employee_bk
+            AND sale.date::DATE >= d_employee._valid_from_datetime
+            AND sale.date::DATE < d_employee._valid_to_datetime
         LEFT JOIN d_region
             ON COALESCE(sale.region, 'Unknown') = d_region.region_name
     WHERE
